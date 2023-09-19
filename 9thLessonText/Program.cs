d15dcc03e23ad20c128dcf698bb73df2daac7c62
Yurii-Shevchuk <yuriishvchk@gmail.com>
2023-09-19 19:05:42 +0300

    Refactored almost every method

--- 9thLessonText/Program.cs
+++ 9thLessonText/Program.cs
@@ -3,45 +3,49 @@ using System.Text.RegularExpressions;
 
 namespace Lesson_9_Text_And_Files
 {
-    internal class Program
+    internal static class Program
     {
         static string database = "./../../../db.csv";
         static (string name, string phone, DateTime birth)[] contacts;
 
         static void Main(string[] args)
         {
-            // string absolute_path_to_file = @"C:\Users\alexl\source\repos\C_Sharp_107\Lesson_9_Text_And_Files\document.txt";
-            // string relative_path_to_file = @"./../../../document_2.txt";
-
-            // string text = File.ReadAllText(absolute_path_to_file, Encoding.UTF8);
-            // string[] lines = File.ReadAllLines(relative_path_to_file);
-
-            // 0. SAVE IT TO THE FILE WITH ".CSV"
-            // 1. Writes to console currently available contacts in the file
-            // 2. Add new contact
-            // 3. Edit contact
-            // 4. Search contacts
-            // 5. Calculates the contact age
-            // 6. Save database
-
             string[] records = ReadDatabaseAllTextLines(database);
             contacts = ConvertStringsToContacts(records);
-
-            while (true)
+            bool isExit = false;
+            while (!isExit)
             {
-                UserInteraction();
+                UserInteraction(ref isExit);
             }
+            Console.WriteLine("Bye-bye!");
         }
 
-        static void UserInteraction()
+        static void UserInteraction(ref bool isExit)
         {
             Console.WriteLine("1. Write all contacts");
             Console.WriteLine("2. Add new contact");
             Console.WriteLine("3. Edit contact");
-            Console.WriteLine("4. Search by row number"); //TODO search by name
-            Console.WriteLine("6. Save");
-
-            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine("4. Search by name or phone number"); 
+            Console.WriteLine("5. Save");
+            Console.WriteLine("6. Sort by name");
+            Console.WriteLine("7. Exit the program");
+            int input;
+            try
+            {
+             input = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Your input was incorrect, please try again");
+                UserInteraction(ref isExit);
+                return;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("There is no possible way this program could have THIS many functions, try better");
+                UserInteraction(ref isExit);
+                return;
+            }
             switch (input)
             {
                 case 1:
@@ -56,9 +60,15 @@ namespace Lesson_9_Text_And_Files
                 case 4:
                     SearchContact();
                     break;
-                case 6:
+                case 5:
                     SaveContactsToFile();
                     break;
+                case 6:
+                    SortContacts();
+                    break;
+                case 7:
+                    isExit = true;
+                    return;
                 default:
                     Console.WriteLine("No such operation.");
                     break;
@@ -68,13 +78,28 @@ namespace Lesson_9_Text_And_Files
         static void AddNewContact()
         {
             Console.Write("Please, enter your name: ");
-            string newName = Console.ReadLine();
+            string newName = Console.ReadLine().Trim();
+            newName = newName == "" ? "John" : newName;
+
             Console.Write("Please, enter your phone number (starting from 380): ");
             string newPhoneNumber = FormatPhoneNumber(Console.ReadLine());
-            Console.Write("Please, enter your date of birth (dd.mm.yyyy format): ");
-            DateTime newDate = DateTime.Parse(Console.ReadLine());
-            var newEntry = (newName, newPhoneNumber, newDate);
 
+            Console.Write("Please, enter your date of birth (dd.mm.yyyy format): ");
+            DateTime newDate;
+            try
+            {
+                newDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You made a mistake during date input, we'll set up placeholder date so that you can change it via edit function");
+            }
+            finally
+            {
+                newDate = DateTime.ParseExact("01.01.2000", "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture);
+            }
+
+            var newEntry = (newName, newPhoneNumber, newDate);
             var newContacts = new (string, string, DateTime)[contacts.Length + 1];
             Array.Copy(contacts, newContacts, contacts.Length);
             newContacts[newContacts.Length - 1] = newEntry;
@@ -83,28 +108,79 @@ namespace Lesson_9_Text_And_Files
 
         static void EditContact()
         {
-            int contactRowTochange = SearchContact();
+            int contactRowTochange;
+            do
+            {
+                contactRowTochange = SearchContact();
+            }while( contactRowTochange < 0 );
+
             Console.Write("Please, enter new name: ");
-            string newName = Console.ReadLine();
+            string newName = Console.ReadLine().Trim();
+            newName = newName != "" ? newName : contacts[contactRowTochange].name;
+
             Console.Write("Please, enter new phone number (starting from 380): ");
             string newPhoneNumber = FormatPhoneNumber(Console.ReadLine());
+
             Console.Write("Please, enter new date of birth (dd.mm.yyyy format): ");
-            DateTime newDate = DateTime.Parse(Console.ReadLine());
+            DateTime newDate;
+            try
+            {
+                newDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You made a mistake during date input, we'll set up placeholder date so that you can change it via edit function");
+            }
+            finally
+            {
+                newDate = DateTime.ParseExact("01.01.2000", "dd.MM.yyyy", System.Globalization.CultureInfo.CurrentCulture);
+            }
+
             var newEntry = (newName, newPhoneNumber, newDate);
             contacts[contactRowTochange] = newEntry;
         }
 
         static int SearchContact()
         {
-            int rowNumber;
-            Console.Write("Enter the number of row you're looking for:");
-            do
+            if(contacts.Length == 0)
             {
-                rowNumber = int.Parse(Console.ReadLine());
-            } while (rowNumber < contacts.Length);
-            var contact = contacts[rowNumber-1];
-            WriteOneContactToConsole(contact);
-            return rowNumber-1;
+                Console.WriteLine("There are no contacts, go add a new one");
+                AddNewContact();
+            }
+
+            int rowNumber;
+            string userInput;
+
+            Console.Write("Enter the name or phone number you're looking for:");
+            userInput = Console.ReadLine().Trim();
+            for (rowNumber = 0; rowNumber < contacts.Length; rowNumber++)
+            {
+                if (contacts[rowNumber].name.Contains(userInput, StringComparison.OrdinalIgnoreCase) || contacts[rowNumber].phone.Contains(userInput))
+                {
+                    var contact = contacts[rowNumber];
+                    WriteOneContactToConsole(contact);
+                    return rowNumber;
+                }
+            }
+            return -1;
+        }
+
+        static void SortContacts()
+        {
+            for (int i = 0; i < contacts.Length - 1; i++)
+            {
+                int minIdx = i;
+                for(int j = i + 1; j < contacts.Length; j++)
+                {
+                    if (String.Compare(contacts[j].name, contacts[minIdx].name, StringComparison.OrdinalIgnoreCase) < 0)
+                    {
+                        minIdx = j;
+                    }
+                }
+                (contacts[i], contacts[minIdx]) = (contacts[minIdx], contacts[i]);
+            }
+            Console.WriteLine("Here's sorted list of contacts");
+            WriteAllContactsToConsole();
         }
 
         static void WriteOneContactToConsole((string name, string phone, DateTime date) contact)
@@ -123,9 +199,6 @@ namespace Lesson_9_Text_And_Files
 
         static (string name, string phone, DateTime date)[] ConvertStringsToContacts(string[] records)
         {
-            // records:
-            // "name,phone,date of birth"
-            // Oleksii,+38090873928,30.03.1993
             var contacts = new (string name, string phone, DateTime date)[records.Length];
             for (int i = 0; i < records.Length; ++i)
             {
@@ -161,14 +234,27 @@ namespace Lesson_9_Text_And_Files
             return File.ReadAllLines(file);
         }
 
-        static string FormatPhoneNumber(string phoneNumber )
+        static string FormatPhoneNumber(string phoneNumber)
         {
             if(!phoneNumber.StartsWith('+'))
             {
                 phoneNumber = "+" + phoneNumber;
             }
-            string pattern = @"(\d{3})(\d{2})(\d{3})(\d{4})";
 
+            string formattedPattern = @"\+*\(\d{3}\)\d{2}-\d{3}-\d{4}";
+
+            if ((phoneNumber.Length > 13 && !Regex.IsMatch(phoneNumber, formattedPattern)) || phoneNumber.Length <13)
+            {
+                Console.WriteLine("This app supports numbers that fit into 12 numbers, for now have the placeholder number and edit it, if you need");
+                phoneNumber = "+380111111111";
+            }
+
+            if(Regex.IsMatch(phoneNumber, formattedPattern))
+            {
+                return phoneNumber;
+            }
+            string pattern = @"(\d{3})(\d{2})(\d{3})(\d{4})";
+            
             string formattedPhoneNumber = Regex.Replace(phoneNumber, pattern, "($1)$2-$3-$4");
             return formattedPhoneNumber;
         }
